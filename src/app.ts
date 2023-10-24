c8c2df1d96247453ab9711b6a753024120f5d2b8
shakhawatsalam <shakhawatsalam726@gmail.com>
2023-10-24 09:21:37 +0600

    [cloudniry setup is done]

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,33 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 const app: Application = express();
-const port = 3000;
+import cookieParser from 'cookie-parser';
+import config from './config';
 
-app.use(cors());
+app.use(
+  cors({
+    origin: config.origin,
+  }),
+);
 
 // * parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// *
+app.use(cookieParser());
+
 // * Testing
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
+});
+
+// * unknown route
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
 });
 
 export default app;
--- src/config/index.ts
+++ src/config/index.ts
@@ -6,4 +6,11 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  origin: process.env.ORIGIN,
+  redisurl: process.env.REDIS_URL,
+  cloudinary: {
+    cloudname: process.env.CLOUD_NAME,
+    cloudapikey: process.env.CLOUD_API_KEY,
+    cloudsecretkey: process.env.CLOUD_SECRET_KEY,
+  },
 };
